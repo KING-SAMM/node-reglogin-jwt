8ad383ec98e437ceb9c58eb652deac67a96843b1
KING-Samm <kcsamm11@gmail.com>
2022-03-05 19:48:42 +0100

    Added registraion form with data sending functionality

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 
 const app = express();
